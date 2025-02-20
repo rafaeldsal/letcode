4e243bd3667ba2753b6010e01e043f5db14d1327
Rafael Souza <rafaelsnt123@gmail.com>
2025-02-20 15:56:18 -0300

    Desafio 1036 - [Fórmula de Bhaskara]

--- formula-de-bhaskara/src/Main.java
+++ formula-de-bhaskara/src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+
+        double r1;
+        double r2;
+
+        if (A == 0) {
+            System.out.println("Impossivel calcular");
+        } else {
+            double delta = delta(A, B, C);
+            if (delta > 0 || delta == 0) {
+                r1 = (-B + delta) / (2 * A);
+                r2 = (-B - delta) / (2 * A);
+
+                System.out.printf("R1 = %.5f%n", r1);
+                System.out.printf("R2 = %.5f%n", r2);
+            } else {
+                System.out.println("Impossivel calcular");
+            }
         }
+
+        sc.close();
+    }
+
+    public static double delta(double A, double B, double C) {
+        return Math.sqrt(B * B - 4 * A * C);
     }
 }
